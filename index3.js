d5a4c0cb0d82d7acc19a1fd60551059bfc514a0e
tchindayvana <tchindayvana83@gmail.com>
2024-07-14 04:52:06 -0300

    amelioration de mon portfolio et rotation de mon image

--- index3.js
+++ index3.js
@@ -13,4 +13,30 @@ image.addEventListener("click", function(){
 image.addEventListener("mouseover",function(){
     image.classList.remove("rotate");
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const bubbleContainer = document.querySelector('.bubble-container');
 
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    function createBubble() {
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+        bubble.style.left = `${Math.random() * 100}%`;
+        bubble.style.animationDuration = `${Math.random() * 10 + 5}s`;
+        bubble.style.backgroundColor = getRandomColor(); // Set random color
+        bubbleContainer.appendChild(bubble);
+
+        setTimeout(() => {
+            bubble.remove();
+        }, 15000);
+    }
+
+    setInterval(createBubble, 500);
+});
